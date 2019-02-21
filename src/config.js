4a1ddbaa6dcd610b544f64b83db3b5b548a3ca84
Gauthier PONTET <gauthiernpn@users.noreply.github.com>
2019-02-21 01:01:33 +0100

    Configuring environments

--- src/config.js
+++ src/config.js
@@ -1,36 +1,34 @@
 const dev = {
-  STRIPE_KEY: "pk_test_v1amvR35uoCNduJfkqGB8RLD",
   s3: {
-    REGION: "us-east-1",
-    BUCKET: "notes-app-2-api-dev-attachmentsbucket-6wbhcogxihbo"
+    REGION: "YOUR_DEV_S3_UPLOADS_BUCKET_REGION",
+    BUCKET: "YOUR_DEV_S3_UPLOADS_BUCKET_NAME"
   },
   apiGateway: {
-    REGION: "us-east-1",
-    URL: "https://api.serverless-stack.seed-demo.club/dev"
+    REGION: "YOUR_DEV_API_GATEWAY_REGION",
+    URL: "YOUR_DEV_API_GATEWAY_URL"
   },
   cognito: {
-    REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_yKOQs2dWK",
-    APP_CLIENT_ID: "1h0r8s763vnqj4id6dvguanp8f",
-    IDENTITY_POOL_ID: "us-east-1:dedfd34a-9d7e-4bf2-a1dd-ef603bac2ecb"
+    REGION: "YOUR_DEV_COGNITO_REGION",
+    USER_POOL_ID: "YOUR_DEV_COGNITO_USER_POOL_ID",
+    APP_CLIENT_ID: "YOUR_DEV_COGNITO_APP_CLIENT_ID",
+    IDENTITY_POOL_ID: "YOUR_DEV_IDENTITY_POOL_ID"
   }
 };
 
 const prod = {
-  STRIPE_KEY: "pk_test_v1amvR35uoCNduJfkqGB8RLD",
   s3: {
-    REGION: "us-east-1",
-    BUCKET: "notes-app-2-api-prod-attachmentsbucket-1h5n5ttet1hy0"
+    REGION: "YOUR_PROD_S3_UPLOADS_BUCKET_REGION",
+    BUCKET: "YOUR_PROD_S3_UPLOADS_BUCKET_NAME"
   },
   apiGateway: {
-    REGION: "us-east-1",
-    URL: "https://api.serverless-stack.seed-demo.club/prod"
+    REGION: "YOUR_PROD_API_GATEWAY_REGION",
+    URL: "YOUR_PROD_API_GATEWAY_URL"
   },
   cognito: {
-    REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_TwYpMXIJH",
-    APP_CLIENT_ID: "6kfg0o7qo2i3ndk2ur906sc5fd",
-    IDENTITY_POOL_ID: "us-east-1:f4c754b4-24f0-4754-8596-30afedece1fc"
+    REGION: "YOUR_PROD_COGNITO_REGION",
+    USER_POOL_ID: "YOUR_PROD_COGNITO_USER_POOL_ID",
+    APP_CLIENT_ID: "YOUR_PROD_COGNITO_APP_CLIENT_ID",
+    IDENTITY_POOL_ID: "YOUR_PROD_IDENTITY_POOL_ID"
   }
 };
 
