b1d4f6f7643a72c85226090e7cccced3564c9237
Gauthier PONTET <gauthiernpn@users.noreply.github.com>
2019-02-22 22:48:54 +0100

    up

--- src/config.js
+++ src/config.js
@@ -17,18 +17,18 @@ const dev = {
 
 const prod = {
   s3: {
-    REGION: "YOUR_PROD_S3_UPLOADS_BUCKET_REGION",
-    BUCKET: "YOUR_PROD_S3_UPLOADS_BUCKET_NAME"
+    REGION: "us-east-1",
+    BUCKET: "notes-app-2-api-dev-attachmentsbucket-citqkeljv53h"
   },
   apiGateway: {
-    REGION: "YOUR_PROD_API_GATEWAY_REGION",
-    URL: "YOUR_PROD_API_GATEWAY_URL"
+    REGION: "us-east-1",
+    URL: "https://yckniowp52.execute-api.us-east-1.amazonaws.com/dev"
   },
   cognito: {
-    REGION: "YOUR_PROD_COGNITO_REGION",
-    USER_POOL_ID: "YOUR_PROD_COGNITO_USER_POOL_ID",
-    APP_CLIENT_ID: "YOUR_PROD_COGNITO_APP_CLIENT_ID",
-    IDENTITY_POOL_ID: "YOUR_PROD_IDENTITY_POOL_ID"
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_WTwXYRfDO",
+    APP_CLIENT_ID: "1hm44744t6o6gp8ldaq7m5uamf",
+    IDENTITY_POOL_ID: "d9e70760-b2f8-4b81-8ede-e81802de3076"
   }
 };
 
